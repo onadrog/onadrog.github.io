57e2dde58ae0814af3aad09cd220d033ce88871c
onadrog <sebastien.gordano@gmail.com>
2023-06-30 12:30:58 +0200

    test: workflow

--- astro.config.ts
+++ astro.config.ts
@@ -8,33 +8,33 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "http://localhost:3000",
-    markdown: {
-        shikiConfig: {
-            theme: "dracula",
-            wrap: true,
-        },
-    },
-    integrations: [
-        sitemap(),
-        image({
-            serviceEntryPoint: "@astrojs/image/sharp",
-        }),
-        compress({
-            css: false,
-            html: {
-                removeComments: true,
-                removeAttributeQuotes: false,
-            },
-            img: false,
-            js: false,
-            svg: true,
-        }),
-        prefetch(),
-    ],
-    vite: {
-        build: {
-            assetsInlineLimit: 0,
-        },
-    },
+	site: "https://onadrog.github.io",
+	markdown: {
+		shikiConfig: {
+			theme: "dracula",
+			wrap: true,
+		},
+	},
+	integrations: [
+		sitemap(),
+		image({
+			serviceEntryPoint: "@astrojs/image/sharp",
+		}),
+		compress({
+			css: false,
+			html: {
+				removeComments: true,
+				removeAttributeQuotes: false,
+			},
+			img: false,
+			js: false,
+			svg: true,
+		}),
+		prefetch(),
+	],
+	vite: {
+		build: {
+			assetsInlineLimit: 0,
+		},
+	},
 });
